8b8e982d585a5b738d9f6d77921f147aaa973752
Zain Sadaqat <zain.sadaqet@gmail.com>
2023-08-09 20:36:33 +0500

    Set up routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,25 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import EventsScreen from './screens/EventsScreen';
+import PageNotFound from './screens/PageNotFoundScreen';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <>Hello Coach!</>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomeScreen />} />
+          <Route path="login" element={<LoginScreen />} />
+          <Route path="register" element={<RegisterScreen />} />
+          <Route path="events" element={<EventsScreen />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
