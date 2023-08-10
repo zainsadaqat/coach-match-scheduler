cf0560d55e032db0c41180c14e56a707d926c0f5
Zain Sadaqat <zain.sadaqet@gmail.com>
2023-08-10 16:44:17 +0500

    Create a sidebar component

---
+++ frontend/src/components/Sidebar.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import MenuItem from './MenuItem';
+import HomeIcon from '../assets/HomeIcon.png';
+import MyTeamIcon from '../assets/MyTeamIcon.png';
+import EventsIcon from '../assets/EventsIcon.png';
+import HelpIcon from '../assets/HelpIcon.png';
+import AppLogo from '../assets/AppLogo.png';
+import SidebarMenu from './SidebarMenu';
+import Logo from './Logo';
+import { Link } from 'react-router-dom';
+
+const Sidebar = () => {
+  return (
+    <nav>
+      {/* Logo */}
+      <Link to="/">
+        <Logo logoImage={AppLogo} appName="Coach Match Scheduler" />
+      </Link>
+
+      {/* Menu items */}
+      <div className="">
+        <SidebarMenu>
+          <div>
+            <Link to="/">
+              <MenuItem iconImage={HomeIcon} menuItem="Home" />
+            </Link>
+            <Link to="/my-team">
+              <MenuItem iconImage={MyTeamIcon} menuItem="My Team" />
+            </Link>
+            <Link to="/events">
+              <MenuItem iconImage={EventsIcon} menuItem="Events" />
+            </Link>
+          </div>
+          <div>
+            <MenuItem iconImage={HelpIcon} menuItem="Help" />
+          </div>
+        </SidebarMenu>
+      </div>
+    </nav>
+  );
+};
+
+export default Sidebar;
