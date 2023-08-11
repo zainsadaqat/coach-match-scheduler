1507e5f27b0d246ced9ac7393f3c4c477ac399db
Zain Sadaqat <zain.sadaqet@gmail.com>
2023-08-11 23:10:48 +0500

    Fix sidebar

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -7,11 +7,12 @@ import HelpIcon from '../assets/HelpIcon.png';
 import AppLogo from '../assets/AppLogo.png';
 import SidebarMenu from './SidebarMenu';
 import Logo from './Logo';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const Sidebar = () => {
+  const { pathname: currentPath } = useLocation();
   return (
-    <nav className="p-2 bg-slate-400">
+    <nav className="p-2 border-x-2">
       {/* Logo */}
       <Link to="/">
         <Logo logoImage={AppLogo} appName="Coach Match Scheduler" />
@@ -22,18 +23,34 @@ const Sidebar = () => {
         <SidebarMenu>
           <div>
             <Link to="/">
-              <MenuItem iconImage={HomeIcon} menuItem="Home" />
+              {currentPath === '/' ? (
+                <MenuItem iconImage={HomeIcon} menuItem="Home" active />
+              ) : (
+                <MenuItem iconImage={HomeIcon} menuItem="Home" />
+              )}
             </Link>
             <Link to="my-team">
-              <MenuItem iconImage={MyTeamIcon} menuItem="My Team" />
+              {currentPath === '/my-team' ? (
+                <MenuItem iconImage={MyTeamIcon} menuItem="My Team" active />
+              ) : (
+                <MenuItem iconImage={MyTeamIcon} menuItem="My Team" />
+              )}
             </Link>
             <Link to="events">
-              <MenuItem iconImage={EventsIcon} menuItem="Events" />
+              {currentPath === '/events' ? (
+                <MenuItem iconImage={EventsIcon} menuItem="Events" active />
+              ) : (
+                <MenuItem iconImage={EventsIcon} menuItem="Events" />
+              )}
             </Link>
           </div>
           <div>
             <Link to="help">
-              <MenuItem iconImage={HelpIcon} menuItem="Help" />
+              {currentPath === '/help' ? (
+                <MenuItem iconImage={HelpIcon} menuItem="Help" active />
+              ) : (
+                <MenuItem iconImage={HelpIcon} menuItem="Help" />
+              )}
             </Link>
           </div>
         </SidebarMenu>
