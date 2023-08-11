f8fc0f0bce2a9a6d39170e9d89de3606e75e937a
Zain Sadaqat <zain.sadaqet@gmail.com>
2023-08-11 23:10:58 +0500

    Fix logo

--- frontend/src/components/Logo.tsx
+++ frontend/src/components/Logo.tsx
@@ -9,9 +9,9 @@ const Logo: React.FC<LogoProps> = ({ logoImage, appName }) => {
   return (
     <React.Fragment>
       <div className="logo-large-screen">
-        <div className="flex items-center justify-around font-semibold my-4">
+        <div className="flex items-center justify-start font-semibold my-4">
           <img src={logoImage} alt={appName} width={28} height={28} />
-          <span className="ml-2 text-[0.85rem]">{appName}</span>
+          <span className="ml-2 text-[1rem]">{appName}</span>
         </div>
       </div>
       <div className="logo-medium-screen">
