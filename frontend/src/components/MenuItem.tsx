aad076d706a03d35c639c7eef47b992b45736340
Zain Sadaqat <zain.sadaqet@gmail.com>
2023-08-11 23:11:11 +0500

    Make menu item responsive

--- frontend/src/components/MenuItem.tsx
+++ frontend/src/components/MenuItem.tsx
@@ -3,13 +3,20 @@ import './MenuItem.css';
 interface MenuItemProps {
   iconImage: string;
   menuItem: string;
+  active?: boolean;
 }
 
-const MenuItem: React.FC<MenuItemProps> = ({ iconImage, menuItem }) => {
+const MenuItem: React.FC<MenuItemProps> = ({ iconImage, menuItem, active }) => {
   return (
     <React.Fragment>
       <div className="menu-item-large-screen">
-        <li className="flex items-center my-4">
+        <li
+          className={
+            active
+              ? `active flex items-center my-4 py-[8px] px-[12px]`
+              : `flex items-center my-4 py-[8px] px-[12px]`
+          }
+        >
           <img src={iconImage} alt={menuItem} width={18} height={18} />
           <span className="ml-[12px] font-semibold font-[14px] hover:underline">
             {menuItem}
