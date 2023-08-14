59adf44dcf9b1f8c899fc9c2186cbfd2a66aa234
Zain Sadaqat <zain.sadaqet@gmail.com>
2023-08-14 23:52:06 +0500

    Update create event screen

--- frontend/src/screens/CreateEventScreen.tsx
+++ frontend/src/screens/CreateEventScreen.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import MyCalendar from '../components/MyCalendar';
+import SaveButton from '../components/SaveButton';
 
 const CreateEventScreen = () => {
   return (
-    <section>
-      <div className="max-w-[800px] p-4">
-        <h2 className="font-semibold text-[24px]">
-          Create an Event (Basic info)
-        </h2>
+    <section className="px-4">
+      <div className="max-w-[600px] p-4">
+        <div className="my-4">
+          <h2 className="font-semibold text-[24px]">Create an Event</h2>
+        </div>
         <form>
           <div className="my-2">
             <label className="font-medium text-[14px]">Title</label>
@@ -25,18 +26,25 @@ const CreateEventScreen = () => {
               placeholder="Category of the event..."
             />
           </div>
-          <h3 className="font-bold text-[1.25rem]">Date & Time</h3>
-          <div>
+          <div className="my-4">
+            <h3 className="font-bold text-[1.25rem]">Date & Time</h3>
+          </div>
+          <div className="my-2">
             <MyCalendar />
           </div>
-          <h3 className="font-bold text-[1.25rem]">Location</h3>
+          <div className="my-4">
+            <h3 className="font-bold text-[1.25rem]">Location</h3>
+          </div>
           <div className="my-2">
             <label className="font-medium text-[14px]">Event will be in</label>
             <input
               className="p-2 w-[100%] border rounded"
               type="text"
-              placeholder="location"
+              placeholder="Enter the event location"
             />
+          </div>
+          <div className="my-8">
+            <SaveButton />
           </div>
         </form>
       </div>
